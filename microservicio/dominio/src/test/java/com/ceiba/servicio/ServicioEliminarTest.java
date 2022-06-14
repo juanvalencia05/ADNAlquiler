e7fa4ad3cdd63541e2fd195dc0b5ec6bc612466b
juanvalenciaceiba <juan.valencia@ceiba.com.co>
2022-06-14 16:53:18 -0500

    Se organiza prueba Unitaria

--- microservicio/dominio/src/test/java/com/ceiba/servicio/ServicioEliminarTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/servicio/ServicioEliminarTest.java
@@ -1,7 +1,6 @@
 package com.ceiba.servicio;
 
 import com.ceiba.BasePrueba;
-import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
 import com.ceiba.puerto.RepositorioAlquiler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,12 +14,23 @@ public class ServicioEliminarTest {
         var repositorio = Mockito.mock(RepositorioAlquiler.class);
         var servicio = new ServicioEliminarAlquiler(repositorio);
 
-        Mockito.when(!repositorio.existe(Mockito.anyInt())).thenReturn(true);
+        Mockito.when(repositorio.existe(Mockito.anyInt())).thenReturn(true);
 
-      //  Assertions.assertEquals("No existe el Alquiler con los datos ingresados",
-                //Assertions.assertThrows(IllegalArgumentException.class,() -> servicio.eliminar(1)).getMessage());
-        BasePrueba.assertThrows(() ->servicio.eliminar(Mockito.anyInt()), IllegalArgumentException.class,"No existe el Alquiler con los datos ingresados");
+      Assertions.assertEquals("No existe el Alquiler con los datos ingresados",
+                Assertions.assertThrows(IllegalArgumentException.class,() -> servicio.eliminar(1)).getMessage());
 
+
+    }
+
+    @Test
+    void deberiaEliminarElAlquiler()
+    {
+        RepositorioAlquiler repositorioAlquiler = Mockito.mock(RepositorioAlquiler.class);
+        Mockito.when(repositorioAlquiler.existe(Mockito.anyInt())).thenReturn(true);
+
+        ServicioEliminarAlquiler servicioEliminarAlquiler = new ServicioEliminarAlquiler(repositorioAlquiler);
+        
+        BasePrueba.assertThrows(()->servicioEliminarAlquiler.eliminar(Mockito.anyInt()),IllegalArgumentException.class, "No existe el Alquiler con los datos ingresados");
     }
 
 }
