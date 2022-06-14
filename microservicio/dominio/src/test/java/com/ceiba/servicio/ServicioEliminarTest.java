55a81e4bf5c4a2fc326b46d2b10b140c01e3cd85
juanvalenciaceiba <juan.valencia@ceiba.com.co>
2022-06-14 14:01:35 -0500

    Se organiza code smells

--- microservicio/dominio/src/test/java/com/ceiba/servicio/ServicioEliminarTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/servicio/ServicioEliminarTest.java
@@ -7,17 +7,5 @@ import org.mockito.Mockito;
 
 public class ServicioEliminarTest {
 
-    @Test
-    void noExistaAlquiler()
-    {
-        var repositorio = Mockito.mock(RepositorioAlquiler.class);
-        var servicio = new ServicioEliminarAlquiler(repositorio);
 
-        Mockito.when(!repositorio.existe(Mockito.any())).thenReturn(true);
-
-        Assertions.assertEquals("No existe el Alquiler con los datos ingresados",
-                Assertions.assertThrows(IllegalArgumentException.class,() -> servicio.eliminar(1)).getMessage());
-
-
-    }
 }
