b495af03005ba9ee3a3812f23208864aa2c8c998
juanvalenciaceiba <juan.valencia@ceiba.com.co>
2022-06-15 10:11:32 -0500

    Prueba unitaria para el servicio Eliminar

--- microservicio/dominio/src/test/java/com/ceiba/servicio/ServicioEliminarAlquilerTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/servicio/ServicioEliminarAlquilerTest.java
@@ -1,6 +1,7 @@
 package com.ceiba.servicio;
 
 import com.ceiba.BasePrueba;
+import com.ceiba.dto.AlquilerResumenDto;
 import com.ceiba.puerto.RepositorioAlquiler;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -8,7 +9,19 @@ import org.mockito.Mockito;
 public class ServicioEliminarAlquilerTest {
 
     @Test
-    void noExistaAlquiler()
+    void existaAlquiler()
+    {
+        AlquilerResumenDto alquilerResumenDto = new AlquilerResumenDto();
+        RepositorioAlquiler repositorioAlquiler = Mockito.mock(RepositorioAlquiler.class);
+        Mockito.when(repositorioAlquiler.consultarPorId(Mockito.anyInt())).thenReturn(alquilerResumenDto);
+
+        ServicioEliminarAlquiler servicioEliminarAlquiler = new ServicioEliminarAlquiler(repositorioAlquiler);
+        servicioEliminarAlquiler.eliminar(1);
+        Mockito.verify(repositorioAlquiler,Mockito.times(1)).eliminar(1);
+    }
+
+    @Test
+    void ExepcionNoExisteAlquier()
     {
         RepositorioAlquiler repositorioAlquiler = Mockito.mock(RepositorioAlquiler.class);
         Mockito.when(repositorioAlquiler.existe(Mockito.anyInt())).thenReturn(false);
