1d78af38cea37e4f68cb5259e4e15c3e6161a39d
juanvalenciaceiba <juan.valencia@ceiba.com.co>
2022-06-15 10:58:04 -0500

    se organizan pruebas unitarias

--- microservicio/dominio/src/test/java/com/ceiba/servicio/ServicioEliminarAlquilerTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/servicio/ServicioEliminarAlquilerTest.java
@@ -1,5 +1,6 @@
 package com.ceiba.servicio;
 
+import com.ceiba.dto.AlquilerResumenDto;
 import com.ceiba.puerto.RepositorioAlquiler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,21 @@ public class ServicioEliminarAlquilerTest {
 
         Assertions.assertEquals("No existe el Alquiler con los datos ingresados",
                 Assertions.assertThrows(IllegalStateException.class,() -> servicio.eliminar(1)).getMessage());
+    }
+
+    @Test
+    void eliminarAlquiler()
+    {
+        AlquilerResumenDto alquilerResumenDto = new AlquilerResumenDto();
+        var repositorio = Mockito.mock(RepositorioAlquiler.class);
+        var servicio = new ServicioEliminarAlquiler(repositorio);
+
+        Mockito.when(repositorio.consultarPorId(Mockito.anyInt())).thenReturn(alquilerResumenDto);
+
+        servicio.eliminar(1);
+
+        Mockito.verify(repositorio,Mockito.times(1)).eliminar(1);
+
     }
 
 }
